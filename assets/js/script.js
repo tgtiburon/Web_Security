04346d428b54e0cec3b3b6db82b290b41cc4e2e5
tgtiburon <tg.tiburon@gmail.com>
2021-11-09 14:37:07 -0500

    VirusTotal can get data for any website

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,21 +4,58 @@ console.log("script.js loaded!");
 
 // Variables
 const vTotalInfo = '8cf0ca0342f8870b2601ff6a6292c366162f55ab28d1213baafd6c4f06a2c53a';
+savedVTID = []; //Holds the saved ID from Virus Total 
+savedVTResults = []; //Holds the results from the URL scan
+
 
 
 
 
 // Functions
 
+/*  Function: initialLoad() 
+    => Called when the website loads to take care of any housekeeping issues.
+    args: none
+    return: none
+*/
 
 const initialLoad = () => {
+    // load saved data so we don't use up api calls
+    loadSavedData();
 
 
 }//end initialLoad()
+
+/*  Function: loadSavedData()
+    => loads all saved Data
+    args: none
+    return: none
+*/
+
+let loadSavedData = function() {
+
+    let savedVTID = localStorage.getItem("savedVTID");
+    //no savedVTID make one
+    if (!savedVTID) {
+        savedVTID = [];
+    } else {
+        // load and parse savedVTID
+       savedVTID = JSON.parse(localStorage.getItem("savedVTID"));
+    
+       console.log("analysisID = " + savedVTID)
+    }
+    
+    //return savedVTID;
+};
+
+/*  Function: webSiteGetID 
+    => fetches the special VirusTotal ID needed to run analyse
+    args: none
+    return: none
+*/
+
 const webSiteGetID  = () => {
 
-  
-   
     let myRequestURL = "https://www.virustotal.com/api/v3/urls";
    
     let formData = new FormData();
@@ -38,38 +75,69 @@ const webSiteGetID  = () => {
            
     }
 
-    console.log("myRequestObject below:")
-    console.log(myRequestObject);
-
    fetch(myRequestURL, myRequestObject).then(function(response){ 
  
     console.log(response);
         if(response.ok) {
           
             response.json().then(function(data) {
-                console.log(data); 
-                console.log(response.status);
-                let analysisID = data.id;
-                console.log("analysisID = " + analysisID)
+             
+                savedVTID = data.data.id;
+                  
+                localStorage.setItem("savedVTID", JSON.stringify(savedVTID));
+                webSiteScan(savedVTID);
             
             });
-
-   
         } else {
-            console.log("It worked!");
-            console.log(response.status);
-            // localStorage.setItem("webSiteID", JSON.stringify(webSiteID));
-
-
+            console.log("WebsiteGetID failed to get an ID!");
         }
     });
 
 
 };//End websiteGetID()
 
-const webSiteScan = () => {
 
-    
+/*  Function: webSiteScan
+    => Takes the VT url ID and sends it to get analyzed
+    and saves the result to localStorage to cut down on API calls.
+    args: savedVTID
+    return: none
+*/
+const webSiteScan = (savedVTID) => {
+
+    let myRequestURL = "https://www.virustotal.com/api/v3/analyses/" + savedVTID;
+
+    let myHeaders = new Headers();
+    myHeaders = {"X-Apikey" : vTotalInfo };
+  
+    myRequestObject = {
+        method: 'GET',
+        headers: myHeaders,
+        mode: 'cors'
+        
+    }
+    console.log(myRequestURL);
+
+   fetch(myRequestURL, myRequestObject).then(function(response){ 
+ 
+    console.log(response);
+        if(response.ok) {
+        
+            response.json().then(function(data) {
+           
+                savedVTResults = data;
+               
+                localStorage.setItem("savedVTResults", JSON.stringify(savedVTResults));
+            
+            });
+
+        } else {
+            console.log("It failed!");
+           
+        }
+        console.log(response);
+    });
+   
 }// end webSiteScan
 
 
