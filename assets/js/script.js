d7b4bbc6803be3d726ba413a79c603d42d28f8a4
tgtiburon <tg.tiburon@gmail.com>
2021-11-09 08:19:23 -0500

    testing to see if it works on the server, due to origin null on pc

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,150 +4,112 @@ console.log("script.js loaded!");
 
 // Variables
 
-vTotalInfo = "8cf0ca0342f8870b2601ff6a6292c366162f55ab28d1213baafd6c4f06a2c53a";
+const vTotalInfo = '8cf0ca0342f8870b2601ff6a6292c366162f55ab28d1213baafd6c4f06a2c53a';
 
 
 
 
 // Functions
-const startUp  = () => {
+const webSiteScan  = () => {
 
- //   curl --request POST 
- // --url https://www.virustotal.com/api/v3/urls 
- // --header 'x-apikey: 8cf0ca0342f8870b2601ff6a6292c366162f55ab28d1213baafd6c4f06a2c53a' 
- // --form url='www.google.com'
-
-
+  
  
-  letTmpObj = curl --request POST \
-  --url https://www.virustotal.com/api/v3/urls \
-  --header 'x-apikey: 8cf0ca0342f8870b2601ff6a6292c366162f55ab28d1213baafd6c4f06a2c53a' \
-  --form url='www.google.com'
+
+// winner winner chicken dinner
+//curl --request POST --url https://www.virustotal.com/api/v3/urls --header "x-apikey: 8cf0ca0342f8870b2601ff6a6292c366162f55ab28d1213baafd6c4f06a2c53a" --form url="www.google.com"
+
+//https://www.virustotal.com/vtapi/v2/url/report?apikey=8cf0ca0342f8870b2601ff6a6292c366162f55ab28d1213baafd6c4f06a2c53a&resource='www.google.com'
 
 
-
-
-   // curl --request POST --url https://www.virustotal.com/vtapi/v2/url/scan --header 'x-apikey: $8cf0ca0342f8870b2601ff6a6292c366162f55ab28d1213baafd6c4f06a2c53a' --form url= 'https://tines.io'
+    let myRequestURL = "https://www.virustotal.com/api/v3/urls";
+   
+    console.log(vTotalInfo);
   
-   // curl --POST --https://www.virustotal.com/vtapi/v2/url/scan --'x-apikey: $8cf0ca0342f8870b2601ff6a6292c366162f55ab28d1213baafd6c4f06a2c53a' --url= 'https://tines.io'
-  
+
+ //debugger;
+ //tmpStr = JSON.stringify("www.google.com");
+ let formData = new FormData()
+ formData.append('url', 'www.google.com');
+ myRequestObject = {
+     method: 'POST',
+    // headers: {'x-apikey' : vTotalInfo},
+    headers: {'x-apikey' :'8cf0ca0342f8870b2601ff6a6292c366162f55ab28d1213baafd6c4f06a2c53a' },
+     body: formData,
+     mode: 'no-cors',
+     Credentials : 'same-origin'
+     
+    
+ }
+
+ //origin = "www.mozilla.org";
+ console.log(myRequestObject);
+
+   fetch(myRequestURL, myRequestObject).then(function(response){ 
+   // fetch(myRequestURL, {method:'POST', headers:{'x-apikey' : vTotalInfo  }, body:{'url':'www.google.com'}, mode:'no-cors', credentials: 'same-origin'}).then(function(response) {
+
+        if(response.ok) {
+            response.json().then(function(data) {
+                console.log(data); 
+                console.log(response.status);
+            
+        });
+
+   
+        } else {
+            alert("Error:  Web Security failed to retrieve");
+            console.log(response.status);
 
 
-  console.log("after curl");
-
-    //letTempObj = 
-
+        }
+    });
 
 
-   // cityName = city;
-      
-    //let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial" + "&appid="  + yekIPA;
-    //let tmpObj = curl --request POST --url https://www.virustotal.com/vtapi/v2/url/scan --data 'apikey=' + vtTotalInfo + --data 'url=www.google.com';
+//version 2.0
 
-   // curl  --url "https://www.virustotal.com/api/v3/urls" --header 'x-apikey:' + vt+  --form url='www.google.com'
-   // curl  POST https://www.virustotal.com/api/v3/urls' + vtTotalInfo + 'www.google.com'
-   // curl  https://www.virustotal.com/api/v3/urls<8cf0ca0342f8870b2601ff6a6292c366162f55ab28d1213baafd6c4f06a2c53a>''www.google.com'
-   // curl https://www.virustotal.com/api/v3/urls x-apikey:'8cf0ca0342f8870b2601ff6a6292c366162f55ab28d1213baafd6c4f06a2c53a' url='www.google.com'
-    console.log(tmpObj);
+// let formData2 = new FormData()
+//  formData2.append('url', 'www.google.com');
+//  myRequestObject2 = {
+//      method: 'POST',
+//     // headers: {'x-apikey' : vTotalInfo},
+//     headers: {'apikey' :'8cf0ca0342f8870b2601ff6a6292c366162f55ab28d1213baafd6c4f06a2c53a'},
+//      body: formData2,
+//      mode: 'no-cors'
+//  }
+//  console.log(myRequestObject2);
 
+//  let myRequestURL2 = "https://www.virustotal.com/vtapi/v2/url/scan";
 
-   // curl --request POST --url https://www.virustotal.com/api/v3/urls --header 'x-apikey: 8cf0ca0342f8870b2601ff6a6292c366162f55ab28d1213baafd6c4f06a2c53a' --form url='www.google.com'
+//    fetch(myRequestURL2, myRequestObject2).then(function(response){ 
+//    // fetch(myRequestURL, {method:'POST', headers:{'x-apikey' : vTotalInfo  }, body:{'url':'www.google.com'}, mode:'no-cors', credentials: 'same-origin'}).then(function(response) {
 
+//         if(response.ok) {
+//             response.json().then(function(data) {
+//                 console.log(data); 
+//                 console.log(response.status);
+            
+//         });
 
-   // fetch(apiUrl).then(function(response) {
-     //   if(response.ok) {
-     //       response.json().then(function(data) {
-               // displayRepos(data.items, language);
-       //        coords = data;
-       //        console.log(data);
-              // localStorage.setItem("coords", JSON.stringify(data));
-
-              // let lon = data.coord.lon;
-               //let lat = data.coord.lat;
-              // console.log("lat=" + lat + " lon= " + lon);
-
-
-               // Use one Call API to get all the data we need
-              
-             //  getWeather(lon, lat);
-
-       //     });
-    //    } else {
-    //        alert("Error:  Open Weather Failed");
-    //    }
-  //  });
+   
+//         } else {
+//             alert("Error:  Web Security failed to retrieve");
+//             console.log(response.status);
 
 
+//         }
+//     });
 };
 
 
 
-// const getCoords = (city) => {
-//     debugger;
-    
-//        // debugging
-//        cityName = city;
-      
-//         let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial" + "&appid="  + yekIPA;
-    
-//         fetch(apiUrl).then(function(response) {
-//             if(response.ok) {
-//                 response.json().then(function(data) {
-//                    // displayRepos(data.items, language);
-//                    coords = data;
-//                    console.log(data);
-//                    localStorage.setItem("coords", JSON.stringify(data));
-    
-//                    let lon = data.coord.lon;
-//                    let lat = data.coord.lat;
-//                    console.log("lat=" + lat + " lon= " + lon);
-    
-    
-//                    // Use one Call API to get all the data we need
-                  
-//                    getWeather(lon, lat);
-    
-//                 });
-//             } else {
-//                 alert("Error:  Open Weather Failed");
-//             }
-//         });
-//     };
-//     // Use coordinates to get all the weather data needed.
-//     const getWeather = (lon,lat) => {
-//         debugger;
-    
-//         // using the one call api from openweathermap we can get everything we need.
-//         let apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat  + "&lon=" + lon 
-//                      + "&units=imperial" + "&exclude=minutely,hourly,alerts"  + "&appid="  + yekIPA;
-    
-//         fetch(apiUrl).then(function(response) {
-//             if(response.ok) {
-//                 response.json().then(function(data) {
-//                    // displayRepos(data.items, language);
-//                    let weatherData = data;
-//                    console.log(data);
-//                    localStorage.setItem("weatherData", JSON.stringify(data));
-//                    // get the lon and lat so I can call the one call api
-    
-//                    readWeatherData(weatherData);
-                     
-//                 });
-//             } else {
-//                 alert("Error:  Open Weather Failed");
-//             }
-//         });
-    
-    
-//     }
-    
+
+
 
 
 
 // Function calls
 
 
-startUp();
+webSiteScan();
 
 
 
