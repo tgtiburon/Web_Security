bd350a9fc76396840473647032074935d2d3ede1
tgtiburon <tg.tiburon@gmail.com>
2021-11-13 17:52:43 -0500

    deleted code associated with api's we are not using

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,7 +36,6 @@ const initialLoad = () => {
 
 let loadSavedData = function() {
     
-
     let savedVTID = localStorage.getItem("savedVTID");
     //no savedVTID make one
     if (!savedVTID) {
@@ -47,7 +46,6 @@ let loadSavedData = function() {
         // We use the ID by sending it to virustotal, which it uses to analyze the url.
        savedVTID = JSON.parse(localStorage.getItem("savedVTID"));
     
-       //console.log("savedVTID = " + savedVTID)
     }
 
     savedVTResults = localStorage.getItem("savedVTResults");
@@ -58,11 +56,9 @@ let loadSavedData = function() {
         // load and parse savedVTResults -- This is the saved results from virus
         // total after we sent it for analysis.
        let savedVTResults = JSON.parse(localStorage.getItem("savedVTResults"));
-       // console.log("savedVTResults = "  );
-      //  console.log(savedVTResults);
         processVTData(savedVTResults);
     }
-   // finalResultsObjArr
+ 
     finalResultsObjArr = localStorage.getItem("finalResultsObjArr");
     //no savedVTResults
     if (!finalResultsObjArr) {
@@ -70,9 +66,7 @@ let loadSavedData = function() {
     }else {
         // load and parse savedVTResults
        let finalResultsObjArr = JSON.parse(localStorage.getItem("finalResultsObjArr"));
-      //  console.log("finalResultsObjArr = "  );
-       // console.log(finalResultsObjArr);
-        
+       
     }
 
    
@@ -95,11 +89,7 @@ const webSiteGetID  = (/*url*/) => {
     // set up the headers
     let myHeaders = new Headers();
     myHeaders = {"X-Apikey" : vTotalInfo };
-    //myHeaders = {"Content-Type" : "application/json", "API-Key" : yourVaribleForAPI};
-    // debug info
-    //console.log(formData);
-    //console.log(myHeaders);
-    //Create the myRequestObject
+   
     myRequestObject = {
         method: 'POST',
         headers: myHeaders,
@@ -116,7 +106,6 @@ const webSiteGetID  = (/*url*/) => {
             response.json().then(function(data) {
 
                 // Virus total sends us the ID of the URL 
-             
                 savedVTID = data.data.id;
                   
                 localStorage.setItem("savedVTID", JSON.stringify(savedVTID));
@@ -130,7 +119,6 @@ const webSiteGetID  = (/*url*/) => {
             console.log("WebsiteGetID failed to get an ID!");
         }
     });
-
 
 };//End websiteGetID()
 
@@ -153,12 +141,10 @@ const webSiteScan = (savedVTID) => {
         headers: myHeaders,
         mode: 'cors'    
     }
-    // for debugging
-   // console.log(myRequestURL);
+
     // try and fetch the analysis of the url
    fetch(myRequestURL, myRequestObject).then(function(response){ 
  
-    //console.log(response);
         if(response.ok) {
             // it worked parse and store the data
         
@@ -169,156 +155,55 @@ const webSiteScan = (savedVTID) => {
                 localStorage.setItem("savedVTResults", JSON.stringify(savedVTResults));
 
                 // Time to process all that data
-                processVTData(savedVTResults);
-            
+                processVTData(savedVTResults);     
             });
 
         } else {
             // it failed.
             console.log("It failed!");
            
-        }
-        //console.log(response);
-    });
-   
+        }  
+    });  
 }// end webSiteScan
 
-const urlAjax = () => {
-    //debugger;
-
-     // curl -H "Content-Type: application/json" -H "API-Key: 4929f2c5-ed32-477f-b97e-bf05771c34a5" "https://urlscan.io/user/quotas/"
-     tmpAPI = "4929f2c5-ed32-477f-b97e-bf05771c34a5";
-     // website we want to scan.  We will have input box later
-    // let myRequestURL = "https:urlscan.io/api/v1/scan/";
-    let myRequestURL = "https://urlscan.io/api/v1/search/?query=domain:urlscan.io";
-
-      // need to submit as a FormData object
-    let formData = new FormData();
-    // formData.append('url', 'www.google.com');
-    // formData.append("visibility", "public");
- 
-     formData= {"url":"www.google.com", "visibility":"public"};
-     // set up the headers
-     let myHeaders = new Headers();
-    //myHeaders = { "API-Key" : tmpAPI };
- 
-   myHeaders = { "Content-Type": "application/json","API-Key" : tmpAPI };
-   // myHeaders = { "Content-Type": "application/json","API-Key" : tmpAPI, "Access-Control-Allow-Origin": "http://127.0.0.1:5500", "Access-Control-Allow-Credentials": "true" };
-   // myHeaders = { "Content-Type": "application/json","API-Key" : tmpAPI, "Access-Control-Allow-Origin": "http://127.0.0.1:5500", "Access-Control-Allow-Credentials": "true" };
 
 
-    // myHeaders = {"Content-Type" : "application/json", "API-Key" : yourVaribleForAPI};
-     // debug info
-     //console.log(formData);
-     //console.log(myHeaders);
-     //Create the myRequestObject
-     console.log("below is formData");
-     console.log( formData);
-     myRequestObject = {
-        // dataType: 'jsonp',
-         method: 'POST',
-         headers:myHeaders,
-         data: formData,
-         body:formData,
-        // url: formData,
-       // body: myRequestURL,
-      //credentials: 'anonymous',
-      //referrerPolicy: 'origin-when-cross-origin',
-        mode: 'cors' 
-           
-     }
-     console.log(myRequestObject);
+// Code to scan ny times technology articles
+const storyScan = () => {
 
-    // $.post(myRequestURL, myRequestObject, function(data) {
-        $.post(myRequestURL,myRequestObject, function(data) {
-         console.log("It worked");
-         console.log(data);
+    tmpAPI = "A7QoqgMwCbe99GKVGJdTY3zisUsBXdAl";
+  
+    const url9 = "https://api.nytimes.com/svc/news/v3/content/all/technology.json?api-key="+tmpAPI;
 
+   const options = {
+     method: "GET",
+     headers: {
+       "Accept": "application/json"
+     },
+   };
+   fetch(url9, options).then(
+     response => {
+       if (response.ok) {
+         return response.json();
+       }
+       return response.text().then(err => {
+         return Promise.reject({
+           status: response.status,
+           statusText: response.statusText,
+           errorMessage: err,
+         });
+       });
      })
-     .done(function() {
-         console.log("second success");
+     .then(data => {
+       console.log(data);
      })
-     .fail (function() {
-         console.log("fail");
-
-     })
-     .always(function() {
-         console.log("finished");
+     .catch(err => {
+       console.error(err);
      });
-
-
-}
-const urlScanIO = () => {
-
-    // curl -H "Content-Type: application/json" -H "API-Key: 4929f2c5-ed32-477f-b97e-bf05771c34a5" "https://urlscan.io/user/quotas/"
-    tmpAPI = "4929f2c5-ed32-477f-b97e-bf05771c34a5";
-    // website we want to scan.  We will have input box later
-    let myRequestURL = "https:urlscan.io/api/v1/scan/";
-
-   // need to submit as a FormData object
-    let formData = new FormData();
-   // formData.append('url', 'www.google.com');
-   // formData.append("visibility", "public");
-
-    formData= '{"url":"www.google.com", "visibility":"public"}';
-    // set up the headers
-    let myHeaders = new Headers();
-
-    myHeaders = { "Content-Type": "application/json","API-Key" : tmpAPI };
-    //myHeaders = {"Content-Type" : "application/json", "API-Key" : yourVaribleForAPI};
-    // debug info
-    //console.log(formData);
-    //console.log(myHeaders);
-    //Create the myRequestObject
-    console.log( formData);
-    myRequestObject = {
-        method: 'POST',
-        headers:myHeaders,
-        data: formData,
-      // body:formData,
-       // url: formData,
-      // body: myRequestURL,
-     // credentials: 'include',
-     //referrerPolicy: 'origin-when-cross-origin',
-        mode: 'no-cors' 
-          
-    }
-    console.log(myRequestObject);
-   // Try and fetch the id of the website
-   //fetch(myRequestURL, myRequestObject).then(function(response){ 
-     //  debugger;
-   // fetch( myRequestURL, myRequestObject).then(function(response){ 
-        // test
-     //   debugger;
-        fetch("https://urlscan.io/api/v1/search/?q=domain:urlscan.io").then(function(response){ 
-
-    console.log(response);
-        if(response.ok) {
-          // it worked so save the id
-            response.json().then(function(data) {
-
-                // Virus total sends us the ID of the URL 
-                console.log("Got good response");
-                console.log(data);
-             
-               // savedVTID = data.data.id;
-                  
-               // localStorage.setItem("savedVTID", JSON.stringify(savedVTID));
-                // Now we send the special ID virus total sent us, back to them to analyze
-                // I am not sure why they don't do it all in one step.  
-               // webSiteScan(savedVTID);
-            
-            });
-        } else {
-            //it failed
-            console.log("WebsiteGetID failed to get an ID!");
-        }
-    });
+ }
 
 
 
-
-}
 /*  Function: processVTData
     => Takes the data from the analyze call at VT
     and puts it into easy to use objects or array??
@@ -331,15 +216,12 @@ processVTData = (savedVTResults) => {
     // storing the savedVTResults sent to the function into a variable with 
     // the same name
     savedVTResults = savedVTResults;
-
-    //console.log(savedVTResults.data.attributes.results);
     // We are turning the results into a temporary object so we can 
     // use forEach to loop through it later.
     let tmpObj = savedVTResults.data.attributes.results
-  
     // iterator for the Object loop
-   let i = 0;
-   let finalResultsObjArr = [];
+    let i = 0;
+    let finalResultsObjArr = [];
 
     // Using forEach to go through the object array so we can store the results in an easier format
     Object.values(tmpObj).forEach(val=> {
@@ -348,14 +230,9 @@ processVTData = (savedVTResults) => {
         finalResultsObjArr.push({engine:val.engine_name, verdict:val.result});
         i++;
     });
-
-    //console.log(finalResultsObjArr);
     // lets save the new object array.
     // saved as   Engine Name :  Result   
     localStorage.setItem("finalResultsObjArr", JSON.stringify(finalResultsObjArr));
-
-   
-
 }
 // Label the input button with id="inputButton" so
 // it can be tied to this.
@@ -366,11 +243,6 @@ $("body").on("click", "#inputButton", function() {
 
 // Function calls
 initialLoad();// Call this to start the website.
-
-
-
-
-// Listeners --We will probably use jquery so won't need them.
 
 
 
